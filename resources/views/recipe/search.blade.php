0a155d8d726eafa9bb1235cc3f96e769d0c7e2e4
”kanami” <ray.knm1116@gmail.com>
2020-08-06 14:06:18 +0000

    bladeの作成・編集

---
+++ resources/views/recipe/search.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('title', 'my recipes')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h1>検索結果</h1>
+            </div>
+        </div>
+    </div>
+@endsection
