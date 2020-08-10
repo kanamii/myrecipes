5f28ec357b74bbd8133f21e54e382f1e440e361c
”kanami” <ray.knm1116@gmail.com>
2020-08-10 08:42:55 +0000

    コントローラーの作成

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class RecipeController extends Controller
 {
+    //topページを表示させる
+    
+    //member個別ページを表示させる
+    
+    //レシピ個別ページを表示させる
+    
+    //レシピ一覧を表示させる
+    
+    //カテゴリー別一覧を表示させる
+    
+    //検索結果を表示させる
+    
     //
 }
