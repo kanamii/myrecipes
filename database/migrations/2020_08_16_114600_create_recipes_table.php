bf3743af7897189bab80932a9bf419072b3a6ef8
”kanami” <ray.knm1116@gmail.com>
2020-10-24 08:22:08 +0000

    デプロイ対応

--- database/migrations/2020_08_16_114600_create_recipes_table.php
+++ database/migrations/2020_08_16_114600_create_recipes_table.php
@@ -15,6 +15,7 @@ class CreateRecipesTable extends Migration
     {
         Schema::create('recipes', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->int('user_id');
             $table->string('name');
             $table->string('category');
             $table->string('introduction');
