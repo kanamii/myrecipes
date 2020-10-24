9564496e3a4dcfb4d96d8ffe27d4f89e94d36756
”kanami” <ray.knm1116@gmail.com>
2020-10-24 07:50:49 +0000

    投稿後リダイレクト変更

--- app/Http/Controllers/Admin/RecipeController.php
+++ app/Http/Controllers/Admin/RecipeController.php
@@ -51,8 +51,10 @@ class RecipeController extends Controller
       $recipe->save();
       
       $user_id = $recipe->user_id;
-
-      return redirect()->route('member', ['id' => $user_id]);
+      
+      $posts = Recipe::latest()->paginate(8);
+      return redirect()->route('hone', ['posts' => $posts]);
+      // return redirect()->route('member', ['id' => $user_id]);
   }
   
   // レシピ編集画面を開く
